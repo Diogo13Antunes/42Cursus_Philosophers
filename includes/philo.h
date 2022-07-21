ede5e3b661ac98f7297705fedea118f15a85dbfc
Diogo Antunes <diogoantunes@Bocals-iMac.local>
2022-07-21 11:07:51 +0100

    updated philo.h

--- includes/philo.h
+++ includes/philo.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dcandeia <dcandeia@student.42.fr>          +#+  +:+       +#+        */
+/*   By: diogoantunes <diogoantunes@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/13 14:53:07 by dcandeia          #+#    #+#             */
-/*   Updated: 2022/04/21 17:26:31 by dcandeia         ###   ########.fr       */
+/*   Updated: 2022/07/21 10:28:10 by diogoantune      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,45 +25,23 @@ Arguments
 //================================ Includes ====================================
 # include <stdio.h>
 # include <string.h>
-# include <stdlib.h>
 # include <unistd.h>
+# include <stdlib.h>
 # include <sys/time.h>
 # include <pthread.h>
 //================================= Defines ====================================
 # define TRUE	1
 # define FALSE	0
 //================================= Structs ====================================
-typedef pthread_t		t_thread;
-typedef pthread_mutex_t	t_mutex;
-typedef struct timeval	t_timer;
 
-typedef struct s_philo
-{
-	t_thread	phil;
-	int			is_alive;
-	int			last_meal;
-	t_mutex		*fork_left;
-	t_mutex		*fork_right;
-	int			eat_counter;
-}				t_philo;
-typedef struct s_arg
-{
-	int			nbr_philo;
-	int			time_die;
-	int			time_eat;
-	int			time_sleep;
-	int			nbr_eats;
-	int			game_over;
-	int			time_value;
-	t_philo		*philos;
-	t_mutex		mutex_ph;
-}				t_arg;
 //================================ Prototypes ==================================
-int		ft_atoi(const char *str);
-void	errors(char *str);
-void	check_args(char **argv);
-void	get_data(t_arg *data, int argc, char **argv);
-void	*init_philos(t_arg *args);
+
+/* error_handler.c */
+void	put_error_msg(char *msg);
+
+/* check_arguments.c */
+int		check_arguments(int ac, char **av);
+
 //==============================================================================
 
 #endif
