1c8f859522bccf863605aa0b526382192a0ccf20
Diogo Antunes <diogoantunes@Bocals-iMac.local>
2022-08-04 12:35:27 +0100

    added norminette checker script

---
+++ norm.sh
@@ -0,0 +1,13 @@
+/Users/diogoantunes/norminette/venv/bin/norminette | egrep -B1 'Error|Warning' >check_norm
+
+printf "\x1b[33m[NORMINETTE]: \x1b[0m"
+
+if [ -s check_norm ]
+then
+	printf "\033[1;31m[KO]\x1b[0m\n"
+	cat check_norm | egrep 'Error|Warning'
+else
+	printf "\033[1;32m[OK]\x1b[0m\n"
+fi
+
+rm check_norm
