3d39bb34697ddab268cd781f8778c961eea59fe5
Diogo <diogoandrecandeias@gmail.com>
2022-09-06 11:07:32 +0100

    updated error handler tester script

--- error_testing.sh
+++ error_testing.sh
@@ -41,7 +41,22 @@ printf "\033[0;97mTest -> ./philo 123 ba15ta 25 laranja\n\033[0m"
 ./philo 123 ba15ta 25 laranja
 printf "\n"
 
-printf "\033[0;31mWith Numbers and Letters 💩\n\033[0m"
+printf "\033[0;31mWith numbers biggers than max int 💩\n\033[0m"
 printf "\033[0;97mTest -> ./philo 5 2147483648 3147483647 214748\n\033[0m"
 ./philo 5 2147483648 3147483647 214748
+printf "\n"
+
+printf "\033[0;31mWith numbers and letters 💩\n\033[0m"
+printf "\033[0;97mTest -> ./philo 5 1 1-23 123 123\n\033[0m"
+./philo 5 1 1-23 123 123
+printf "\n"
+
+printf "\033[0;31mWith numbers and letters 💩\n\033[0m"
+printf "\033[0;97mTest -> ./philo 5 1 123- 123 123\n\033[0m"
+./philo 5 1 123- 123 123
+printf "\n"
+
+printf "\033[0;31mWith numbers and letters 💩\n\033[0m"
+printf "\033[0;97mTest -> ./philo 5 1 123a 123 123\n\033[0m"
+./philo 5 1 123a 123 123
 printf "\n"
