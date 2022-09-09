74b27f0ccb6a3fa95e7f95817031bf19aa5ee558
Diogo <diogoandrecandeias@gmail.com>
2022-09-09 17:30:39 +0100

    comment functions and added function to main

--- sources/main.c
+++ sources/main.c
@@ -3,47 +3,47 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dcandeia <dcandeia@student.42.fr>          +#+  +:+       +#+        */
+/*   By: piriquito <piriquito@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 17:03:52 by diogoantune       #+#    #+#             */
-/*   Updated: 2022/08/31 16:01:49 by dcandeia         ###   ########.fr       */
+/*   Updated: 2022/09/09 17:17:12 by piriquito        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
-static void	print_args(char **args)
-{
-	printf("================== ARGS ==================\n\n");
-	printf("Number of Philos = [ %s ]\n", args[1]);
-	printf("Time to die      = [ %s ]\n", args[2]);
-	printf("Time to eat      = [ %s ]\n", args[3]);
-	printf("Time of sleep    = [ %s ]\n", args[4]);
-	if (args[5])
-		printf("Number of eats   = [ %s ]\n", args[5]);
-	printf("\n==========================================\n");
-	printf("\n");
-}
-
-// static void	print_philos(t_philos *philos, int nbr_philos)
+// static void	print_args(char **args)
 // {
-// 	int	i;
-
-// 	i = 0;
+// 	printf("================== ARGS ==================\n\n");
+// 	printf("Number of Philos = [ %s ]\n", args[1]);
+// 	printf("Time to die      = [ %s ]\n", args[2]);
+// 	printf("Time to eat      = [ %s ]\n", args[3]);
+// 	printf("Time of sleep    = [ %s ]\n", args[4]);
+// 	if (args[5])
+// 		printf("Number of eats   = [ %s ]\n", args[5]);
+// 	printf("\n==========================================\n");
 // 	printf("\n");
-// 	printf("================== PHILOS ==================\n\n");
-// 	while (i < nbr_philos)
-// 	{
-// 		printf("[%d] ", philos[i].id);
-// 		printf("[%d] ", philos[i].t_life);
-// 		printf("[%d] ", philos[i].t_eat);
-// 		printf("[%d] ", philos[i].t_sleep);
-// 		printf("[%d] ", philos[i].nbr_eats);
-// 		printf("\n");
-// 		i++;
-// 	}
-// 	printf("\n============================================\n");
 // }
+
+static void	print_philos(t_philos *philos, int nbr_philos)
+{
+	int	i;
+
+	i = 0;
+	printf("\n");
+	printf("================== PHILOS ==================\n\n");
+	while (i < nbr_philos)
+	{
+		printf("[%d] ", philos[i].id);
+		printf("[%d] ", philos[i].t_life);
+		printf("[%d] ", philos[i].t_eat);
+		printf("[%d] ", philos[i].t_sleep);
+		printf("[%d] ", philos[i].nbr_eats);
+		printf("\n");
+		i++;
+	}
+	printf("\n============================================\n");
+}
 
 int	main(int ac, char **av)
 {
@@ -52,8 +52,8 @@ int	main(int ac, char **av)
 
 	if (check_arguments(ac, av))
 		return (-1);
-	print_args(av);
 	set_data(&data, av);
 	philosopher = start_philos(data);
+	print_philos(philosopher, ft_atoi(av[1]));
 	philosopher = init_threads(philosopher, ft_atoi(av[1]));
 }
