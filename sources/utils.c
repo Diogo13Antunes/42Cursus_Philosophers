66178642d3381881e5807afb1e1bfc238d00d332
Diogo andre Candeias antunes <dcandeia@c1r19s2.42lisboa.com>
2022-10-26 14:41:08 +0100

    correction of died philos

--- sources/utils.c
+++ sources/utils.c
@@ -6,7 +6,7 @@
 /*   By: dcandeia <dcandeia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 11:15:26 by piriquito         #+#    #+#             */
-/*   Updated: 2022/10/25 11:00:58 by dcandeia         ###   ########.fr       */
+/*   Updated: 2022/10/26 13:35:17 by dcandeia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,34 @@ void	ft_putstr_fd(char *str, int fd)
 	}
 }
 
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i])
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
+}
+
 void	print_philos_actions(t_philos *philo, char *action)
 {
 	unsigned long	time;
 
 	pthread_mutex_lock(philo->main_locker);
-	time = get_current_time() - philo->data.init_time;
-	printf("%ld %d %s\n", time, philo->id, action);
+	if (*philo->is_alive == ALIVE)
+	{
+		time = get_current_time() - philo->data.init_time;
+		printf("%ld %d %s\n", time, philo->id, action);
+	}
+	else if (!ft_strcmp(action, ACTION_DIED))
+	{
+		time = get_current_time() - philo->data.init_time;
+		printf("%ld %d %s\n", time, philo->id, action);
+	}
 	pthread_mutex_unlock(philo->main_locker);
 }
