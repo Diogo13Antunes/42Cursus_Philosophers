edb2496358aa5a5c0ccee7ba136fe03140d9fbed
Diogo andre Candeias antunes <dcandeia@c1r19s2.42lisboa.com>
2022-10-26 11:25:55 +0100

    corrected free memory

--- sources/alloc_mem.c
+++ sources/alloc_mem.c
@@ -6,47 +6,22 @@
 /*   By: dcandeia <dcandeia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/21 12:42:20 by dcandeia          #+#    #+#             */
-/*   Updated: 2022/10/25 10:53:33 by dcandeia         ###   ########.fr       */
+/*   Updated: 2022/10/26 11:25:13 by dcandeia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
-
-// int	init_alloc_memory(t_philos **phi)
-// {
-// 	pthread_mutex_t	*mutex;
-// 	int				*wait_initialize;
-// 	int				*is_alive;
-
-// 	is_alive = malloc(sizeof(int));
-// 	wait_initialize = malloc(sizeof(int));
-// 	mutex = malloc(sizeof(pthread_mutex_t));
-// 	if (!is_alive || !wait_initialize || !mutex)
-// 	{
-// 		free_all_pointers(is_alive, wait_initialize, mutex);
-// 		return (0);
-// 	}
-// 	if (pthread_mutex_init(mutex, NULL))
-// 	{
-// 		free_all_pointers(mutex, is_alive, wait_initialize);
-// 		return (0);
-// 	}
-// 	set_alloc_mem(phi, is_alive, wait_initialize, mutex);
-// 	return (1);
-// }
 
 int	free_allocated_memory(t_philos *philos)
 {
 	int	i;
 
 	i = -1;
+	pthread_mutex_destroy(philos->main_locker);
 	free_all_pointers(philos->init_timer_bool, philos->is_alive,
 		philos->main_locker);
-	pthread_mutex_destroy(philos->main_locker);
 	while (++i < philos->data.nbr_philos)
-	{
 		pthread_mutex_destroy(&philos[i].right.lock);
-		//free(&philos[i].right.lock);
-	}
+	free(philos);
 	return (0);
 }
